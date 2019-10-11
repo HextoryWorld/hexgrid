0607a0305bcdc2388c59e8823865f08fea2e726d
JM <jmperez@thethings.io>
2019-10-12 01:40:43 +0200

    bootstrap containers, button and modal

--- resources/js/source.js
+++ resources/js/source.js
@@ -1,16 +1,19 @@
 window.onload = function() {
-    const app = new PIXI.Application({ transparent: true });
+    const canvas = document.getElementById("canvas");
+    const app = new PIXI.Application({ width:$("#grid").width(), height: $(window).height()-100,transparent: true, view: canvas});
     const graphics = new PIXI.Graphics();
 
-    const Hex = Honeycomb.extendHex({ size: 5 });
+    const Hex = Honeycomb.extendHex({ size: 20 });
     const Grid = Honeycomb.defineGrid(Hex);
 
-    document.body.appendChild(app.view);
+    console.log($(window).height());
+    console.log($(window).width());
+
     // set a line style of 1px wide and color #999
     graphics.lineStyle(1, 0x999999);
 
     // render 10,000 hexes
-    Grid.rectangle({ width: 100, height: 100 }).forEach(hex => {
+    Grid.rectangle({ width: 32, height: 20 }).forEach(hex => {
         const point = hex.toPoint();
         // add the hex's position to each of its corner points
         const corners = hex.corners().map(corner => corner.add(point));
